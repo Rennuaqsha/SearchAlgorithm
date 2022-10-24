723f520d3a4e8196d2c0576387f030b4008474b1
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-24 17:27:08 +0800

    membuat method untuk input elemen array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -14,7 +14,28 @@ namespace SearchAlgorithm
 
         public void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should heve minimum 1 and maximum 20 elements. \n");
+            }
 
+            //accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("  Enter Array Elements  ");
+            Console.WriteLine("------------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + i + 1 + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
 
     }
