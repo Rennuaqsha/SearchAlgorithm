2cb7995475c1648d2d787570fadc4ca2f23c4710
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-24 17:14:03 +0800

    create class Program

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+
+namespace SearchAlgorithm
+{
+    class program
+    {
+        //Arrat to be searched
+        int[] arr = new int[20];
+        //number of elements in the array 
+        int n;
+        // get the number of elements to store in the array 
+        int i;
+
+        public void input()
+        {
+
+        }
+
+    }
+}
